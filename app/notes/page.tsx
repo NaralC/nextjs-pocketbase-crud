479c620a6ae8578043206c561e17184630e437a4
Naral <77269201+NaralC@users.noreply.github.com>
2022-12-03 16:39:02 +0000

    fetch data from pocketbase

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,37 +1,40 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 async function getNotes() {
-    const response = await fetch(`http://127.0.0.1:8090/api/collections/notes/records?paqge=1&perPage=30`)
-    const data = await response.json()
-    return data?.items as any[]
+  const response = await fetch(
+    `http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30`,
+    { cache: "no-store" }
+  );
+  const data = await response.json();
+  return data?.items as any[];
 }
 
 export default async function HomePage() {
-    const notes = await getNotes()
+  const notes = await getNotes();
 
   return (
     <div>
-        <h1>Notes</h1>
-        {notes.map((note) => {
-            return <Note key={note.id} note={note} />
-        })}
+      <h1>Notes</h1>
+      {notes.map((note) => {
+        return <Note key={note.id} note={note} />;
+      })}
     </div>
-  )
+  );
 }
 
 function Note({ note }: any) {
-    const { title, content, id, created } = note || {};
+  const { title, content, id, created } = note || {};
 
-    return (
-        <>
-        <Link href={`/notes/${id}`}>
-            <div>
-                <h2>{title}</h2>
-                <h5>{content}</h5>
-                <p>{created}</p>
-            </div>
-        </Link>
-        </>
-    )
+  return (
+    <>
+      <Link href={`/notes/${id}`}>
+        <div>
+          <h2>{title}</h2>
+          <h5>{content}</h5>
+          <p>{created}</p>
+        </div>
+      </Link>
+    </>
+  );
 }
